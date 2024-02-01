6970a9a1b5c42d05fbf341bc1b82ab1f862718e0
oliveira533 <go609127@gmail.com>
2024-02-01 18:04:56 -0300

    Estuando programação orientada a objetos

---
+++ POO/ex1.java
@@ -0,0 +1,77 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class ex1{ 
+    public static void main(String args[]){
+
+        Carro carro1 = new Carro();
+        carro1.setModelo("Gol");
+        carro1.setPlaca("EXB-1F48");
+        carro1.setChassi("FHSALKJFHDSLKJHHA");
+        carro1.setCor("Preto");
+        carro1.setMontadora("Volkswagem");
+
+        carro1.mostrar();
+
+        Carro carro2 = new Carro();
+        carro2.setModelo("T-CROSS");
+        carro2.setPlaca("");
+    }
+}
+
+class Carro{
+    private String modelo;
+    private String placa;
+    private String chassi;
+    private String cor;
+    private String montadora;
+    private int km = 0;
+
+    public void mostrar(){
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Placa: " + placa);
+        System.out.println("Chassi: " + chassi);
+        System.out.println("Cor: " + cor);
+        System.out.println("Montadora: " + montadora);
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public String getPlaca(){
+        return placa;
+    }
+
+    public void setPlaca(String placa){
+        this.placa = placa;
+    }
+
+    public String getChassi(){
+        return chassi;
+    }
+
+    public void setChassi(String chassi){
+        this.chassi = chassi;
+    }
+
+    public String getCor(){
+        return cor;
+    }
+
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+
+    public String getMontadora(){
+        return montadora;
+    }
+
+    public void setMontadora(String montadora){
+        this.montadora = montadora;
+    }
+}
