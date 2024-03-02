fdd68266e0bb0c749a8df3f1b01a634097cd6462
oliveira533 <go609127@gmail.com>
2024-03-02 15:36:16 -0300

    Lista ligada

--- Estrutura de dados/lista_encadeada/ls.java
+++ Estrutura de dados/lista_encadeada/ls.java
@@ -9,6 +9,11 @@ public class ls {
         System.out.println("Primeiro: " + lEstado.getPrimeiro().getValor());
         System.out.println("Segundo: " + lEstado.getPrimeiro().getProximo().getValor());
         System.out.println("Último: " + lEstado.getUltimo().getValor());
+        System.out.println("Pesquisa: " + lEstado.get(0));
+        lEstado.Remover("SP");
+        System.out.println("Primeiro: " + lEstado.getPrimeiro().getValor());
+        System.out.println("Segundo: " + lEstado.getPrimeiro().getProximo().getValor());
+        System.out.println("Último: " + lEstado.getUltimo().getValor());
     }
 }
 
@@ -18,30 +23,36 @@ class Elemento{
     // Valor do elemento
     private String sValor;
 
-    // Próximo valor
+    // Próximo valor da lista
     private Elemento sProximo;
 
+    // Metodo para criar novo valor
     public Elemento (String sNewValor){
         this.sValor = sNewValor;
     }
 
+    // Pegar o valor de um elemento
     public String getValor(){
         return sValor;
     }
 
+    // Definir o valor de um elemento
     public void setValor(String sNewValor){
         this.sValor = sNewValor;
     }
 
+    // Pegar o próximo valor de um elemento
     public Elemento getProximo(){
         return sProximo;
     }
 
+    // Definir o próximo elemento na lista
     public void setProximo( Elemento sNewProximo){
         this.sProximo = sNewProximo;
     }
 }
 
+// Classe da lista ligada
 class ListaLig {
     private Elemento primeiro;
     private Elemento ultimo;
@@ -79,23 +90,46 @@ class ListaLig {
             this.nTamanho++;
         }
         else{
-            ultimo.setProximo(eNewElemento);;
-            ultimo = eNewElemento;
+            this.ultimo.setProximo(eNewElemento);
+            this.ultimo = eNewElemento;
         }
+        this.nTamanho++;
 
     }
 
     public void Remover(String sRemove){
-        
+        Elemento eAnterior = null;
+        Elemento eAtual = this.primeiro;
+        System.out.println(eAtual.getValor());
+        System.out.println(sRemove);
+        for(int i = 0; i < this.nTamanho; i++){
+            if(eAtual.getValor().equalsIgnoreCase(sRemove)) {
+                if (eAtual == this.primeiro) {
+                    this.primeiro = eAtual.getProximo();
+                    eAtual.setProximo(null);
+                } else if (eAnterior == this.ultimo) {
+                    this.ultimo = eAnterior;
+                    this.ultimo.setProximo(null);
+                } else{
+                    eAnterior.setProximo(eAtual.getProximo());
+                    eAtual= null;
+                }
+                nTamanho--;
+                break;
+            }
+            eAnterior = eAtual;
+            eAtual = eAtual.getProximo();
+        }
     }
 
-    public Elemento Pesquisar(String sValor){
-        if (this.primeiro.getProximo() == null) {
-            return this.ultimo;
-        }
-        else{
-            
-        }
-        return null;
+    public Elemento get(int posicao){
+        Elemento eAtual = this.primeiro;
+        for(int i=0; i< posicao; i++){
+            if (eAtual.getProximo()!=null) {
+                eAtual= eAtual.getProximo();
+            }
+        };
+
+        return eAtual;
     }
 }
